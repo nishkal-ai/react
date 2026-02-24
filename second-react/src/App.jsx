43afd72f76df6826f311cb49f8c1b1680d37f0a9
NISHKAL SHUKLA <nishkalshukla628@gmail.com>
2026-02-24 04:40:21 +0000

    learn to use react

---
+++ second-react/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <div>
+      <h1>
+        hey this is nishkal
+      
+      </h1>
+     
+      <h2>
+        this is second line 
+      </h2>
+      <User />
+      <Sum />
+    </div>
+  )
+}
+
+
+function User()
+{
+  return(
+    <h1>inside the user component</h1>
+  )
+}
+
+function Sum()
+{
+  return(10+10)
+  
+}
+export default App
